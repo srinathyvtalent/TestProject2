8f084466fa32b8e61a6e1b0dd68457173bc464fe
srinathyvtalent <58939169+srinathyvtalent@users.noreply.github.com>
2019-12-20 10:29:36 +0530

    Update HomeController.cs

--- TestProject2/TestProject2/Controllers/HomeController.cs
+++ TestProject2/TestProject2/Controllers/HomeController.cs
@@ -13,5 +13,9 @@ namespace TestProject2.Controllers
         {
             return View();
         }
+         public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
